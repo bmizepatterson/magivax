e36b73eccb8a22f9d98d8a049d7377fe905f36a2
Ben Patterson <bmizepatterson@gmail.com>
2018-08-06 10:30:42 -0400

    Add lib.php with functions for creating outer HTML structure

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quattrocento+Sans">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" type="text/css" href="magivax.css">
-	<title>Magivax</title>
-</head>
-<body class="brown">
-		<div class="w3-center w3-white w3-display-middle w3-animate-opacity" style="padding:48px">
-			<h1 class="w3-jumbo text-blue">Magivax</h1>
-			<p>Magivax is a vaccine schedule generator for physicians.</p>
-			<p>Recommendations are based on CDC guidelines.</p><p><a href="#">Learn more</a> or
-			<a id="getStarted" href="/enterpatientinfo.php" class="w3-button light-blue hover-tan" style="display:inline;margin-left:8px;">Get Started</a></p>
-		</div>
+<?php
+require_once(dirname(__FILE__) . '/lib.php');
+echo open_body(array('brown'));
+?>
+<div class="w3-center w3-white w3-display-middle w3-animate-opacity" style="padding:48px">
+	<h1 class="w3-jumbo text-blue">Magivax</h1>
+	<p>Magivax is a vaccine schedule generator for physicians.</p>
+	<p>Recommendations are based on CDC guidelines.</p><p><a href="#">Learn more</a> or
+	<a id="getStarted" href="enterpatientinfo.php" class="w3-button light-blue hover-tan" style="display:inline;margin-left:8px;">Get Started</a></p>
+</div>
 
-	<div id="color-palette">
-		<div class="w3-row">
-			<div class="w3-display-container w3-col brown palette"><div class="w3-display-middle">#5D5C61</div></div>
-			<div class="w3-display-container w3-col gray palette"><div class="w3-display-middle">#817B82</div></div>
-			<div class="w3-display-container w3-col light-blue palette"><div class="w3-display-middle">#7395AE</div></div>
-			<div class="w3-display-container w3-col blue palette"><div class="w3-display-middle">#557A95</div></div>
-			<div class="w3-display-container w3-col tan palette"><div class="w3-display-middle">#B1A296</div></div>
-		</div>
+<div id="color-palette">
+	<div class="w3-row">
+		<div class="w3-display-container w3-col brown palette"><div class="w3-display-middle">#5D5C61</div></div>
+		<div class="w3-display-container w3-col gray palette"><div class="w3-display-middle">#817B82</div></div>
+		<div class="w3-display-container w3-col light-blue palette"><div class="w3-display-middle">#7395AE</div></div>
+		<div class="w3-display-container w3-col blue palette"><div class="w3-display-middle">#557A95</div></div>
+		<div class="w3-display-container w3-col tan palette"><div class="w3-display-middle">#B1A296</div></div>
 	</div>
-	<script src="magivax.js"></script>
-</body>
-</html>
+</div>
+<script src="magivax.js"></script>
+<?php close_body(); ?>
---
+++ lib.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* General library of functions
+*/
+
+// TO DO: Prevent command line access
+
+function open_body(array $classes) {
+	$class_str = '';
+	if ($classes) {
+		$class_str .= ' class="';
+		foreach ($classes as $class) {
+			$class_str .= $class;
+		}
+		$class_str .= '"';
+	}
+	return '<!DOCTYPE html>
+<html>
+<head>'. standard_head() . "
+</head>
+<body{$class_str}>";
+}
+
+function standard_head() {
+	return <<<EOF
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quattrocento+Sans">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" type="text/css" href="magivax.css">
+EOF;
+}
+
+function close_body() {
+	return "</body>\n</html>";
+}
