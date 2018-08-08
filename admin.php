6a637ce89e9c79247ad802823811d5b74c635933
Ben Patterson <bmizepatterson@gmail.com>
2018-08-08 10:44:37 -0400

    Print a table of all vaccines in the DB.

--- admin.php
+++ admin.php
@@ -1,9 +1,21 @@
 <?php
 require_once(dirname(__FILE__) . '/lib.php');
+$DB = connect_database();
+$stmt = $DB->query('SELECT * FROM vaccine ORDER BY longname');
 open_body('Admin | Magivax');
 user_nav();
 ?>
 <div class="w3-container">
 	<h1>Manage Vaccine Data</h1>
+	<div class="w3-container">
+		<table class="w3-table-all">
+			<tr><th>id</th><th>Long name</th><th>Short name</th></tr>
+<?php
+foreach ($stmt as $row) {
+	echo '<tr><td>' . $row->id . '</td><td>' . $row->longname . '</td><td>' . $row->shortname . '</td></tr>';
+}
+?>
+		</table>
+	</div>
 </div>
 <?php close_body(); ?>
