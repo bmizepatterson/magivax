fbdaf40d319862d5f3135e82de4f7e14b358b0e9
Ben Patterson <bmizepatterson@gmail.com>
2018-09-06 16:18:04 -0400

    Create form for adding a vaccine

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,5 +1,18 @@
 <?php
 
-$vaccines = $DB->selectAll('vaccine');
+// Add a vaccine to the DB
+if (isset($_POST) and isset($_POST['longName']) and isset($_POST['shortName'])) {
+    $DB->insertRecord(
+        'INSERT INTO vaccine (shortname, longname) VALUES (?, ?)', 
+        array(htmlspecialchars($_POST['shortName']), htmlspecialchars($_POST['longName']))
+    );
+}
+
+// Select all vaccines in the DB
+$vaccines = $DB->getAll('vaccine');
 
 require 'views/admin.view.php';
+
+// echo '<pre>';
+// var_dump($_POST);
+// echo '</pre>';
--- views/admin.view.php
+++ views/admin.view.php
@@ -2,7 +2,27 @@
 echo open_body('Admin | Magivax');
 echo user_nav();
 ?>
-<div class="w3-container">
+
+<div class="w3-content w3-section">
+    <div class="w3-card">
+        <div class="w3-container light-blue">
+            <h1>Add a vaccine</h1>
+        </div>
+        <form method="post" name="newVaccine" id="newVaccine" autocomplete="off">
+            <div class="w3-panel">
+                <label for="longName">Long Name</label>
+                <input class="w3-input" type="text" name="longName" id="longName">
+
+                <label for="shortName">Short Name</label>
+                <input class="w3-input" type="text" name="shortName" id="shortName">
+            </div>
+
+            <input class="w3-button w3-block light-blue hover-tan" type="submit" value="Add" name="addVaccine" id="addVaccine">
+        </form>
+    </div>
+</div>
+
+<div class="w3-container w3-section">
 	<h1>Manage Vaccine Data</h1>
 	<div class="w3-container">
 		<table class="w3-table-all">
