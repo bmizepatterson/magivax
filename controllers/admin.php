51af4073bc1f5e56ec79b64a64e283a73b6e6952
Ben Patterson <bmizepatterson@gmail.com>
2018-09-06 19:59:48 -0400

    Provide functionality for editing/updating a vaccine in /admin

--- controllers/admin.php
+++ controllers/admin.php
@@ -2,15 +2,40 @@
 
 if (!empty($_POST)) {
     // Add a vaccine to the DB
-    if (!empty($_POST['longName']) and !empty($_POST['shortName'])) {
+    if (!empty($_POST['addVaccine']) and !empty($_POST['longName']) and !empty($_POST['shortName'])) {
         $DB->insertRecord(
             'INSERT INTO vaccine (shortname, longname) VALUES (?, ?)', 
             array(htmlspecialchars($_POST['shortName']), htmlspecialchars($_POST['longName']))
         );
     }
+    // Update the current vaccine
+    if (!empty($_POST['updateVaccine']) and !empty($_POST['id']) and !empty($_POST['longName']) and !empty($_POST['shortName'])) {
+        $DB->updateRecord(
+            'UPDATE vaccine SET longname=:longname, shortname=:shortname
+             WHERE id = :id',
+            array(
+                ':longname'  => htmlspecialchars($_POST['longName']),
+                ':shortname' => htmlspecialchars($_POST['shortName']),
+                ':id'        => htmlspecialchars($_POST['id'])
+            )
+        );
+    }
 }
+$edit = false;
 if (!empty($_GET)) {
-
+    // Delete a vaccine
+    if (!empty($_GET['delete']) and !empty($_GET['vaccine'])) {
+        $DB->deleteRecord(
+            'DELETE FROM vaccine WHERE id = ?',
+            array($_GET['vaccine'])
+        );
+    }
+    if (!empty($_GET['edit']) and !empty($_GET['vaccine'])) {
+        $edit = $DB->getRecord(
+            'SELECT * FROM vaccine WHERE id = ?',
+            array($_GET['vaccine'])
+        );
+    }
 }
 
 // Select all vaccines in the DB
@@ -20,11 +45,5 @@ echo open_body('Admin | Magivax');
 echo user_nav();
 
 require 'views/admin.view.php';
-
-echo '<pre>';
-var_dump($_POST);
-var_dump($_GET);
-var_dump($_SERVER);
-echo '</pre>';
 
 echo close_body();
--- views/admin.view.php
+++ views/admin.view.php
@@ -1,18 +1,41 @@
 <div class="w3-content w3-section">
     <div class="w3-card">
         <div class="w3-container light-blue">
-            <h1>Add a vaccine</h1>
+            <?php 
+                if ($edit) {
+                    echo "<h1>Update $edit->longname</h1>";
+                } else {
+                    echo '<h1>Add a vaccine</h1>';
+                }
+            ?>
         </div>
-        <form method="post" name="newVaccine" id="newVaccine" autocomplete="off">
+        <form method="post" name="newVaccine" id="newVaccine" autocomplete="off" action="admin">
             <div class="w3-panel">
-                <label for="longName">Long Name</label>
-                <input class="w3-input" type="text" name="longName" id="longName">
+                <p>
+                <label for="longName">Long Name</label>                
+                <?php if ($edit): ?>
+                    <input class="w3-input" type="text" name="longName" id="longName" value="<?= $edit->longname ?>">
+                <?php else: ?>
+                    <input class="w3-input" type="text" name="longName" id="longName">
+                <?php endif ?>
+                </p>
 
+                <p>
                 <label for="shortName">Short Name</label>
-                <input class="w3-input" type="text" name="shortName" id="shortName">
+                <?php if ($edit): ?>
+                    <input class="w3-input" type="text" name="shortName" id="shortName" value="<?= $edit->shortname ?>">
+                <?php else: ?>
+                    <input class="w3-input" type="text" name="shortName" id="shortName">
+                <?php endif ?>
+                </p>
             </div>
 
-            <input class="w3-button w3-block light-blue hover-tan" type="submit" value="Add" name="addVaccine" id="addVaccine">
+            <?php if ($edit): ?>
+                <input type="hidden" name="id" value="<?= $edit->id ?>">
+                <input class="w3-button w3-block light-blue hover-tan" type="submit" value="Update" name="updateVaccine" id="updateVaccine">
+            <?php else: ?>
+                <input class="w3-button w3-block light-blue hover-tan" type="submit" value="Add" name="addVaccine" id="addVaccine">
+            <?php endif ?>
         </form>
     </div>
 </div>
@@ -24,7 +47,8 @@
 			<tr><th>&nbsp;</th><th>Long name</th><th>Short name</th></tr>
                 <?php foreach ($vaccines as $vaccine) {
                     $deleteLink = "<a href=\"admin?delete=1&vaccine={$vaccine->id}\">Delete</a>";
-                    echo "<tr><td>{$deleteLink}</td><td>{$vaccine->longname}</td><td>{$vaccine->shortname}</td></tr>";
+                    $editLink = "<a href=\"admin?edit=1&vaccine={$vaccine->id}\">Edit</a>";
+                    echo "<tr><td>{$deleteLink} / {$editLink}</td><td>{$vaccine->longname}</td><td>{$vaccine->shortname}</td></tr>";
                 } ?>
 		</table>
 	</div>
