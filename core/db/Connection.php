289acda463bb0454547530c79a2d4c14a82897e9
Ben Patterson <bmizepatterson@gmail.com>
2018-09-06 14:52:02 -0400

    Bubble error if PDOException is caught while trying to connect to db

--- core/db/Connection.php
+++ core/db/Connection.php
@@ -5,13 +5,14 @@ class Connection
     public static function make($config) {
         try {
             return new PDO(
-                $config['connection'].';dbname='.$config['database'],
+                $config['connection'].';dbname='.$config['name'],
                 $config['username'],
                 $config['password'],
                 $config['options']
             );
         } catch (PDOException $e) {
-            die('Error connecting to database: ' . $e->getMessage());
+            $msg = 'Error connecting to database: ' . $e->getMessage();
+            throw new Exception($msg);
         }
     }
 }
