a8ba7d58655093f07f9e741b0fe7d70a5c83704a
Ben Patterson <bmizepatterson@gmail.com>
2018-09-06 16:36:59 -0400

    Create Request object for handling information about the current HTTP request

--- core/setup.php
+++ core/setup.php
@@ -5,5 +5,6 @@ require_once('lib.php');
 require_once('db/Connection.php');
 require_once('db/Query.php');
 require_once('Router.php');
+require_once('Request.php');
 
 $DB = new Query(Connection::make($config['database']));
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 require 'core/setup.php';
 require Router::load('routes.php')
-    ->direct(trim($_SERVER['REQUEST_URI'], '/'));
+    ->direct(Request::uri());
 
