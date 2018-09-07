f4d36a7d5b44e6b2e7f469ee058fbdf2e123a7e5
Ben Patterson <bmizepatterson@gmail.com>
2018-09-07 10:20:37 -0400

    Rename enterpatientinfo to start

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 
 $router->define('', 'controllers/index.php');
-$router->define('enterpatientinfo', 'controllers/enterpatientinfo.php');
+$router->define('start', 'controllers/start.php');
 $router->define('admin', 'controllers/admin.php');
 $router->define('about', 'controllers/about.php');
